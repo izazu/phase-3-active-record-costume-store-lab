265f8d559dc44df6881b554a7198b6908c98e98b
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-04-03 18:03:26 +0200

    done

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,10 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base
+    validates :name, presence: true
+    validates :location, presence: true
+    validates :costume_inventory, numericality: { greater_than_or_equal_to: 0 }
+    validates :num_of_employees, numericality: { greater_than_or_equal_to: 0 }
+    validates :is_in_business, inclusion: { in: [true, false] }
+  end
+  
